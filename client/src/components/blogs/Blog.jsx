938fd332541553f930ebc1688ae2abf171cf0a79
jas3333 <accounts1@protonmail.com>
2023-01-10 20:18:47 -0800

    Added markdown to blog display

--- client/src/components/blogs/Blog.jsx
+++ client/src/components/blogs/Blog.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const Blog = ({ title, content, category }) => {
     return (
         <div className='container-col'>
             <h1>{title}</h1>
             <h2>{category}</h2>
-            <p>{content}</p>
+            <ReactMarkdown
+                children={content}
+                components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '');
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                children={String(children).replace(/\n$/, '')}
+                                style={coldarkDark}
+                                language={match[1]}
+                                PreTag='div'
+                                {...props}
+                            />
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        );
+                    },
+                }}
+            ></ReactMarkdown>
         </div>
     );
 };
